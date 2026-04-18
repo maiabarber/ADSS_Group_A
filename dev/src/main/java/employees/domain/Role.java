417ff5e72eca708d1e920d48a8efcb8629a546bc
Yuval Pariente <yuval11299@gmail.com>
2026-04-18 17:08:48 +0300

    fix

--- dev/src/main/java/employees/domain/Role.java
+++ dev/src/main/java/employees/domain/Role.java
@@ -1,5 +1,9 @@
 package employees.domain;
 
+/**
+ * Role enum represents the different roles that an employee can have in the system.
+ * It currently includes CASHIER and STOREKEEPER roles.
+ */
 public enum Role {
     CASHIER,
     STOREKEEPER
