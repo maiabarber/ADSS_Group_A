20a0d0995d3a50c0a187ef950e2a3a62e848a80e
EdenJab <edenjab@post.bgu.ac.il>
2026-04-18 23:37:59 +0300

    updated presentation layer and Main

--- dev/Main.java
+++ dev/Main.java
@@ -1,5 +1,8 @@
+import presentation.DeliveriesUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        DeliveriesUI ui = new DeliveriesUI();
+        ui.start();
     }
 }
--- dev/src/presentation/DeliveriesUI.java
+++ dev/src/presentation/DeliveriesUI.java
@@ -1,16 +1,45 @@
 package presentation;
 
+import java.util.Scanner;
 import service.DeliveriesApplication;
 
 public class DeliveriesUI {
     private DeliveriesApplication deliveriesApplication;
+    private Scanner scanner;
 
-    public DeliveriesUI(){
+    public DeliveriesUI() {
         this.deliveriesApplication = new DeliveriesApplication();
+        this.scanner = new Scanner(System.in);
     }
 
-    public DeliveriesApplication getDeliveriesApplication(){
+    public DeliveriesApplication getDeliveriesApplication() {
         return this.deliveriesApplication;
     }
-    
+
+    public void start() {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("=== Deliveries System ===");
+            System.out.println("1. Show main menu");
+            System.out.println("0. Exit");
+            System.out.print("Choose an option: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("Main menu selected.");
+                    break;
+                case "0":
+                    running = false;
+                    System.out.println("Exiting system...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+
+            System.out.println();
+        }
+    }
 }
