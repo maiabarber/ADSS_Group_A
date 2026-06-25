8f57da1159d9351d6ddf63e9ee369b03ec399ded
maia barber <maia0139@gmail.com>
2026-06-25 21:14:42 +0300

    fixed

--- dev/src/main/java/employees/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package employees.repository;
-
-import employees.domain.Employee;
-
-public interface EmployeeRepository extends Repository<Employee, String> {
-}
--- dev/src/main/java/employees/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package employees.repository;
-
-import employees.domain.User;
-
-public interface UserRepository extends Repository<User, String> {
-}
